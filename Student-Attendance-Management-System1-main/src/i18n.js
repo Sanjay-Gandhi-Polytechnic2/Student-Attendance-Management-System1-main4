15c4f96a349eae131bb54b13f1e8df0e55e784e2
user.name like:"Irshan-123168 <user.email like:"shaikirshan78@gmail.com>
2026-05-26 13:22:35 +0530

    YousufBasha Told To Commit

--- Student-Attendance-Management-System1-main/src/i18n.js
+++ Student-Attendance-Management-System1-main/src/i18n.js
@@ -1,25 +1,23 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-
-import translationEN from '../public/locales/en/translation.json';
-import translationES from '../public/locales/es/translation.json';
-
-const resources = {
-  en: {
-    translation: translationEN,
-  },
-  es: {
-    translation: translationES,
-  },
-};
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import en from "./locales/en.json";
+import hi from "./locales/hi.json";
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
-    lng: 'en',
-    fallbackLng: 'en',
+    resources: {
+      en: {
+        translation: en,
+      },
+      hi: {
+        translation: hi,
+      },
+    },
 
+    fallbackLng: "en",
     interpolation: {
       escapeValue: false,
     },
